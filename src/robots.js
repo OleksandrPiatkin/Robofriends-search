b3b197c0a461d7b0b9fece458f7f079cde079bbe
Alex <Oleksandr.Piatkin@gmx.com>
2019-08-31 02:23:54 +0200

    Card list done

---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+{
+    id: '1',
+    name: 'John Sims',
+    username: 'John_Sims',
+    email: 'john.sims@mailtest.com'
+},
+{
+    id: '2',
+    name: 'Jake Shepherd',
+    username: 'Jake.Shepherd',
+    email: 'ediablo731mad@mailtest.com'
+},
+{
+    id: '3',
+    name: 'Darwin Blackburn',
+    username: 'Blackburn',
+    email: 'sodegaki@mailtest.com'
+},
+{
+    id: '4',
+    name: 'Fernanda Gay',
+    username: 'Fernanda324',
+    email: 'fernandaG@mailtest.com'
+},
+{
+    id: '5',
+    name: 'Audrey Mcdonald',
+    username: 'Audrey',
+    email: 'mcdonald@mailtest.com'
+},
+{
+    id: '6',
+    name: 'Branden Santiago',
+    username: 'SantiagoDeCuba',
+    email: 'bransan@mailtest.com'
+},
+{
+    id: '7',
+    name: 'Semaj Burke',
+    username: 'Semaj-dlejk',
+    email: 'semaj@mailtest.com'
+},
+{
+    id: '8',
+    name: 'Jaidyn Gay',
+    username: 'JaiDee',
+    email: 'jaidy@mailtest.com'
+},
+{
+    id: '9',
+    name: 'Pierre Orozco',
+    username: 'Orozco',
+    email: 'oro@mailtest.com'
+},
+{
+    id: '10',
+    name: 'Emmy Huang',
+    username: 'Emmy',
+    email: 'emmyhuang@mailtest.com'
+},
+{
+    id: '11',
+    name: 'Pierre Barrera',
+    username: 'Barrera',
+    email: 'pierre23@mailtest.com'
+},
+{
+    id: '12',
+    name: 'Craig Rhodes',
+    username: 'Craig_Rhodes',
+    email: 'Rhodes1990@mailtest.com'
+},
+{
+    id: '13',
+    name: 'Jazlyn Keller',
+    username: 'JazlynKeller',
+    email: 'jazlynkeller@mailtest.com'
+}
+]
