12181c523bdc55faff696eff719eafaa31addd8e
Vladislav <vlavel.well@gmail.com>
2020-01-02 22:50:27 +0200

    Components refactoring

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange }) => {
-    return (
-        <div className='pa2'>
-            <input 
-                className='pa3 ba b--blue bg-lightest-blue'
-                type='search' 
-                placeholder='Search robots'
-                onChange={searchChange}
-                />
-        </div>
-    );
-}
+const SearchBox = ({ searchChange }) => (
+    <div className='pa2'>
+        <input 
+            className='pa3 ba b--blue bg-lightest-blue'
+            type='search' 
+            placeholder='Search robots'
+            onChange={searchChange}
+        />
+    </div>
+);
 
 export default SearchBox;
